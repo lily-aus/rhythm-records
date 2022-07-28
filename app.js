72347bee523d41b3e6d111d84b95d26fdb08f297
artemkk <akuryachy@gmail.com>
2022-07-27 19:54:30 -0600

    all

--- app.js
+++ app.js
@@ -5,6 +5,10 @@
 // Express
 var express = require('express');
 var app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 PORT = 34853;
 
 // Database
@@ -22,19 +26,101 @@ app.set('view engine', '.hbs');                 // Tell express to use the handl
 app.get('/', function(req, res)
     {
         res.render('index');
-
     });
 
-app.get('/', function(req, res)
+app.get('/customers', function(req, res)
     {
         res.render('customers');
-
     });
-// app.get('/', function(req, res)
-//     {
-//         res.render('customers');
-//     });
 
+app.get('/orders', function(req, res)
+    {
+        res.render('orders');
+    });
+
+app.get('/artists', function(req, res)
+    {
+        res.render('artists');
+    });
+
+app.get('/albums', function(req, res)
+    {
+        res.render('albums');
+    });
+
+app.get('/genres', function(req, res)
+    {
+        res.render('genres');
+    });
+
+app.get('/genres_has_albums', function(req, res)
+    {
+        res.render('genres_has_albums');
+    });
+
+app.get('/artists_has_albums', function(req, res)
+    {
+        res.render('artists_has_albums');
+    });
+
+app.get('/orders_has_albums', function(req, res)
+    {
+        let query1 = "SELECT * FROM Orders_has_Albums";
+
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('orders_has_albums', {data: rows});
+        });
+    });
+
+app.get('/insert_customers', function(req, res)
+    {
+        res.render('insert_customers');
+    });
+
+app.get('/update_customers', function(req, res)
+    {
+        res.render('update_customers');
+    });
+
+app.get('/insert_orders', function(req, res)
+    {
+        res.render('insert_orders');
+    });
+
+app.get('/update_orders', function(req, res)
+    {
+        res.render('update_orders');
+    });
+
+app.get('/insert_genres', function(req, res)
+    {
+        res.render('insert_genres');
+    });
+
+app.get('/update_genres', function(req, res)
+    {
+        res.render('update_genres');
+    });
+
+app.get('/insert_artists', function(req, res)
+    {
+        res.render('insert_artists');
+    });
+
+app.get('/update_artists', function(req, res)
+    {
+        res.render('update_artists');
+    });
+
+app.get('/insert_albums', function(req, res)
+    {
+        res.render('insert_albums');
+    });
+
+app.get('/update_albums', function(req, res)
+    {
+        res.render('update_albums');
+    });
 /*
     LISTENER
 */
